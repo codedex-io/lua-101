5f60457ad0af78bb6150004ad7653678a42290cf
Sonny Li <sonnynomnom@gmail.com>
2025-11-17 23:32:40 -0500

    Update 07-boss-battle.lua

--- 2-control-flow-and-loops/07-boss-battle.lua
+++ 2-control-flow-and-loops/07-boss-battle.lua
@@ -9,7 +9,7 @@ hasKey = false
 completedQuest = true
 
 if isAlive and not isStunned and health > 10 and (hasKey or completedQuest) then
-    print("Your boss battle has begun!")
+  print("Your boss battle has begun!")
 else
-    print("You cannot enter the boss battle.")
+  print("You cannot enter the boss battle.")
 end
