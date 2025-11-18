5ad677e91a96037428249b709ef14bcffb5d1e54
Sonny Li <sonnynomnom@gmail.com>
2025-11-17 23:32:30 -0500

    Update 06-what-if.lua

--- 2-control-flow-and-loops/06-what-if.lua
+++ 2-control-flow-and-loops/06-what-if.lua
@@ -5,13 +5,12 @@
 
 GAME_LANG = 2
 
-
 if GAME_LANG == 1 then
-    print("Welcome to your world, Hero.")
+  print("Welcome to your world, Hero.")
 elseif GAME_LANG == 2 then
-    print("Bienvenido a tu mundo, Héroe.")
+  print("Bienvenido a tu mundo, Héroe.")
 elseif GAME_LANG == 3 then
-    print("Bem-vindo ao seu mundo, Herói.")
+  print("Bem-vindo ao seu mundo, Herói.")
 else
-    print("Select a language to begin.")
+  print("Select a language to begin.")
 end
