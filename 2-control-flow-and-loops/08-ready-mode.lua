94a1d8f2d666837b27075606445c92b9b03181c0
Sonny Li <sonnynomnom@gmail.com>
2025-11-17 23:32:58 -0500

    Update 08-ready-mode.lua

--- 2-control-flow-and-loops/08-ready-mode.lua
+++ 2-control-flow-and-loops/08-ready-mode.lua
@@ -5,12 +5,12 @@ boss_ready = false
 turns = 0
 
 while not boss_ready do
-    print("The boss is preparing...")
-    turns = turns + 1
+  print("The boss is preparing...")
+  turns = turns + 1
 
-    if turns == 5 then
-        boss_ready = true
-    end
+  if turns == 5 then
+    boss_ready = true
+  end
 end
 
 print("The boss has appeared!")
