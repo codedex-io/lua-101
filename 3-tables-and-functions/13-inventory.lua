3426a30c6901901b4a4a32792c06a5476e571a56
estrella <epopo2@illinois.edu>
2026-01-07 16:50:51 -0600

    Update Solution 3.13

--- 3-tables-and-functions/13-inventory.lua
+++ 3-tables-and-functions/13-inventory.lua
@@ -2,26 +2,33 @@
 -- Codédex
 
 blocks = {
-    "dirt",
-    "stone",
-    "cobblestone",
-    "wood",
-    "sand",
-    "gravel",
-    "gold ore",
-    "iron ore",
-    "coal ore",
-    "log",
-    "leaves",
-    "sponge",
-    "glass",
-    "lapis lazuli ore"
+    {name = "dirt", rarity = 1},
+    {name = "stone", rarity = 1},
+    {name = "cobblestone", rarity = 1},
+    {name = "wood", rarity = 2},
+    {name = "sand", rarity = 2},
+    {name = "gold ore", rarity = 7},
+    {name = "iron ore", rarity = 4},
+    {name = "coal ore", rarity = 3},
+    {name = "diamond ore", rarity = 9},
+    {name = "emerald ore", rarity = 10},
+    {name = "lapis lazuli ore", rarity = 5}
 }
 
-if #inventory == 0 then
-    print("Your inventory is empty.")
-elseif #inventory <= 5 then
-    print("You have space in your inventory, add more blocks!")
+-- Write code below 💖
+
+rare_count = 0
+
+for i = 1, #blocks do
+    if blocks[i].rarity >= 5 then
+        rare_count = rare_count + 1
+    end
+end
+
+if rare_count > 3 then
+    print("You have valuable items!")
+elseif rare_count >= 1 then
+    print("You have some rare blocks.")
 else
-    print("You have a full inventory.")
+    print("No rare blocks found.")
 end
