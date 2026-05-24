91f60bf15f183c84cf9c9098a0c8ee3ddec272ed
Sonny Li <sonnynomnom@gmail.com>
2026-05-24 01:39:45 -0400

    Update 01-opening-scene.lua

--- 1-ola-lua/01-opening-scene.lua
+++ 1-ola-lua/01-opening-scene.lua
@@ -1,8 +1,6 @@
 -- Opening Scene 🌙
 -- Codédex
 
--- Stardew Valley (2016)
-print("Oh, another day, another mountain of paperwork. There's got to be more to life than this.")
-print("I wish I could just get away from it all...")
-print("Maybe a change of scenery would do me good.")
-print("I've heard there's a place called Pelican Town, nestled in the valley. It sounds peaceful.")
+-- Final Fantasy X (2016)
+print("Listen to my story.")
+print("This... may be our last chance.")
