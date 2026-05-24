fa98f52e07695edb39df2e5d23c77802c88af4e5
Sonny Li <sonnynomnom@gmail.com>
2026-05-24 14:08:01 -0400

    Update 04-title-sequence.lua

--- 1-ola-lua/04-title-sequence.lua
+++ 1-ola-lua/04-title-sequence.lua
@@ -1,22 +1,12 @@
--- Title Sequence
+-- Title Sequence 🎞️
 -- Codédex
 
-name = "Maxie" -- Replace with your name! 
-year = 2025 -- Change to the current year! 
-key = A -- Keep or change the key! 
-
-
-
--- Write code below 💖
+name = "Maxie"
+year = 2025 
+key = "A"
 
 part_credits = "Created by " .. name .. ", " .. year
 part_start_game = "Press " .. key .. " to start"
-
-
-
-
--- DON'T CHANGE! :)
--- These will print your concatenated strings! 
 
 print(part_credits)
 print(part_start_game)
