6b1af308a7613b96d40aee8065e6e8e6a8888d0c
Sonny Li <sonnynomnom@gmail.com>
2026-05-24 01:40:31 -0400

    Update 01-opening-scene.lua

--- 1-ola-lua/01-opening-scene.lua
+++ 1-ola-lua/01-opening-scene.lua
@@ -1,6 +1,6 @@
 -- Opening Scene 🌙
 -- Codédex
 
--- Final Fantasy X (2016)
+-- Final Fantasy X (2001)
 print("Listen to my story.")
-print("This... may be our last chance.")
+print("This may be our last chance.")
